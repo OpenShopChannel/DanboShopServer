28bd38d174576e27ac5e630f8872030e5965881a
dhtdht020 <dhtdht020@gmail.com>
2022-06-21 18:17:23 +0300

    Set nicer download name

--- assets.py
+++ assets.py
@@ -9,16 +9,16 @@ def serve_slug_icon(slug: str) -> Response:
     """Serves an icon for the given slug, returning a missing image if not possible."""
     filename = slug_file_uuid(slug, FileTypes.ICON)
     if os.path.isfile(filename):
-        return send_file(filename, mimetype='image/png')
+        return send_file(filename, mimetype='image/png', download_name=slug+".png")
 
     # Fallback icon
-    return send_file("static/images/missing-app-icon.png", mimetype='image/png')
+    return send_file("static/images/missing-app-icon.png", mimetype='image/png', download_name=slug+".png")
 
 
 def serve_slug_file(slug: str, file_type: FileTypes):
     """Serves the given file type for the passed slug, returning 404 if not possible."""
     filename = slug_file_uuid(slug, file_type)
     if os.path.isfile(filename):
-        return send_file(filename)
+        return send_file(filename, download_name=slug+"."+file_type.value)
     else:
         abort(404)
